0b9411b39906bda4767da71580f281396330460e
Alan Zhang <alanzhangvgc@gmail.com>
2023-01-05 12:51:38 -0800

    Create basic grid of black cells

---
+++ script-main.js
@@ -0,0 +1,23 @@
+let rows = 16
+let cols = 16
+let cellSize = 10
+let width = rows * cellSize
+let height = cols * cellSize
+
+const canvas = document.createElement('div');
+canvas.setAttribute('id', 'canvas');
+canvas.setAttribute('style', `width: ${width}px; height: ${height}px;`)
+
+function createCanvas() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.setAttribute('style', `width: ${cellSize}px; height: ${cellSize}px;`)
+            canvas.appendChild(square);
+        }
+    }
+}
+
+document.body.appendChild(canvas);
+createCanvas()
