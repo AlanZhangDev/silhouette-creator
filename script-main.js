1561d4041500d995a6639ad17dca805abf692dba
Alan Zhang <alanzhangvgc@gmail.com>
2023-01-05 12:59:58 -0800

    Add cell painting mechanic on hover

--- script-main.js
+++ script-main.js
@@ -8,16 +8,22 @@ const canvas = document.createElement('div');
 canvas.setAttribute('id', 'canvas');
 canvas.setAttribute('style', `width: ${width}px; height: ${height}px;`)
 
+function paintCell() {
+    this.classList.add('painted');
+}
+
 function createCanvas() {
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
             const square = document.createElement('div');
             square.classList.add('square');
             square.setAttribute('style', `width: ${cellSize}px; height: ${cellSize}px;`)
+            square.addEventListener('mouseover', paintCell);
             canvas.appendChild(square);
         }
     }
 }
+
 
 document.body.appendChild(canvas);
 createCanvas()
